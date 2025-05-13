128d1dfe4d043b81961977878197908b2575ccd3
song_eun <dmswo546@gmail.com>
2025-05-13 15:38:37 +0900

    Feat: Footer 구현

--- src/layouts/CommonLayout.jsx
+++ src/layouts/CommonLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import styled from 'styled-components'
+import { Footer } from '../components/Footer'
 
 const CommonLayout = () => {
   return (
@@ -8,7 +9,7 @@ const CommonLayout = () => {
       <Wrapper>
         <Outlet />
       </Wrapper>
-      <Nav>내비게이션바</Nav>
+      <Footer />
     </Container>
   )
 }
@@ -28,13 +29,4 @@ const Wrapper = styled.div`
   position: relative;
   padding: 52px 24px 80px 24px;
   border: 1px solid red;
-`
-const Nav = styled.div`
-  border: 1px solid blue;
-  position: absolute;
-  bottom: 0;
-  box-sizing: border-box;
-  width: 100%;
-  max-width: 480px;
-  height: 56px;
 `
