f3eb04cf200a410c1b2a9665b64bc665b969e8c1
leeemingyu <skystar9927@naver.com>
2025-05-15 13:29:29 +0900

    feat: 로그아웃 기능 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import useAuthStore from '../store/useAuthStore'
 
 const MyPage = () => {
-  return <div>MyPage</div>
+  const logout = useAuthStore(state => state.logout)
+  const handleLogoutClick = () => {
+    logout()
+  }
+  return (
+    <div>
+      MyPage
+      <button onClick={handleLogoutClick}>로그아웃</button>
+    </div>
+  )
 }
 
 export default MyPage
